46d9c1307bef0a22049d7f3eeb55c46cd8e4c95b
stphdenis <stph.denis@gmail.com>
2020-07-21 15:58:49 +0000

    first commit

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,22 @@ export const graphqlApiSchema = (args?: GraphQLApiSchemaPluginOptions) => {
   const options: GraphQLApiSchemaPluginOptions = {
     dirName: 'node_modules/@types/typegen-nexus',
     fileName: 'graphqlApiSchema.json',
-    jsonSpace: 2,
     ...args,
   }
 
   return NexusSchema.plugin({
     name: 'nexus-graphql-api-schema-plugin',
     description: 'Generate Nexus Types',
+
     onInstall(builder) {
       GraphQLApiSchema.init(options)
       return {types: []}
     },
-/*    onBeforeBuild(builder) {
+
+   /* onBeforeBuild(builder) {
       GraphQLApiSchema.init(options)
     },*/
+
     onAfterBuild(graphQLSchema) {
       GraphQLApiSchema.setGraphQLSchema(graphQLSchema)
     },
